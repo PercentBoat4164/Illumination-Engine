c3eabe4e3ea473117fa9888dc9bce61ece7de965
PercentBoat4164 <thadd.schelp@gmail.com>
2020-12-21 04:50:39 -0500

    Initial commit

---
+++ Sources/glfw/examples/CMakeFiles/triangle-opengl.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/triangle-opengl.dir/__/deps/glad_gl.c.o"
+  "CMakeFiles/triangle-opengl.dir/triangle-opengl.c.o"
+  "triangle-opengl"
+  "triangle-opengl.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/triangle-opengl.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
