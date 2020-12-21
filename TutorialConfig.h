01f43fc26273b83a0028955c4b8b793fb95583ce
PercentBoat4164 <51725924+PercentBoat4164@users.noreply.github.com>
2020-12-21 04:54:17 -0500

    Delete TutorialConfig.h

--- TutorialConfig.h
+++
@@ -1,8 +0,0 @@
-// the configured options and settings for Tutorial
-#define Tutorial_VERSION_MAJOR 
-#define Tutorial_VERSION_MINOR 
-
-#ifndef RENDERER_CPP_TUTORIALCONFIG_H_IN_H
-#define RENDERER_CPP_TUTORIALCONFIG_H_IN_H
-
-#endif //RENDERER_CPP_TUTORIALCONFIG_H_IN_H
