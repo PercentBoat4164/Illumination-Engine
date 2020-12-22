cccf2812f4772e64d1049906d71694d91b71d1db
PercentBoat4164 <51725924+PercentBoat4164@users.noreply.github.com>
2020-12-22 16:45:25 -0500

    Add files via upload

---
+++ CMakeFiles/GameEngine.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/GameEngine.dir/Renderer.cpp.o"
+  "GameEngine"
+  "GameEngine.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/GameEngine.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
