aadf031986ae373a70d8a70c14be44a10e737224
PercentBoat4164 <thadd.schelp@gmail.com>
2021-04-19 01:46:10 -0400

    added Windows fix for downloading models.

--- DownloadModels.py
+++ DownloadModels.py
@@ -6,12 +6,14 @@ except ImportError:
     print("Installing gdown...")
     os.system("pip3 install gdown")
     print("Please run the program again.")
+    exit()
 try:
     import tarfile
 except ImportError:
     print("Installing tarfile...")
     os.system("pip3 install tarfile")
     print("Please run the program again.")
+    exit()
 gdown.download("https://drive.google.com/uc?id=1m4obHW53rdjeoW_RKvNURFT8cFA3x_jb", "Models.tar.xz", quiet=False)
 print("Download finished\nExtracting...\nFrom: "+os.getcwd()+"Models.tar.xz\nTo: "+os.getcwd()+"/Models")
 file = tarfile.open("Models.tar.xz", "r")
