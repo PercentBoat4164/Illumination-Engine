cf302b99bcd21e47a60c6b26667f121410e79fcf
Thaddeus Schelp <thadd.schelp@gmail.com>
2021-04-19 02:02:16 -0400

    Fixing Windows again.

--- DownloadModels.py
+++ DownloadModels.py
@@ -1,20 +1,15 @@
 import os
 import platform
+import tarfile
+
 try:
     import gdown
 except ImportError:
     print("Installing gdown...")
-    os.system("pip3 install gdown")
+    os.system("python3 -m pip install gdown")
     print("Please run the program again.")
     exit()
-try:
-    import tarfile
-except ImportError:
-    print("Installing tarfile...")
-    os.system("pip3 install tarfile")
-    print("Please run the program again.")
-    exit()
-gdown.download("https://drive.google.com/uc?id=1m4obHW53rdjeoW_RKvNURFT8cFA3x_jb", "Models.tar.xz", quiet=False)
+# gdown.download("https://drive.google.com/uc?id=1m4obHW53rdjeoW_RKvNURFT8cFA3x_jb", "Models.tar.xz", quiet=False)
 print("Download finished\nExtracting...\nFrom: "+os.getcwd()+"Models.tar.xz\nTo: "+os.getcwd()+"/Models")
 file = tarfile.open("Models.tar.xz", "r")
 file.extractall("src/Models")
@@ -22,5 +17,5 @@ print("Cleaning up...\nRemoving: "+os.getcwd()+"/Models.tar.xz")
 if platform.system() == "Linux":
     os.system("rm "+os.getcwd()+"/Models.tar.xz")
 elif platform.system() == "Windows":
-    os.system("del /f "+os.getcwd()+"/Models.tar.xz")
+    os.system("del /f Models.tar.xz")
 print("Done")
