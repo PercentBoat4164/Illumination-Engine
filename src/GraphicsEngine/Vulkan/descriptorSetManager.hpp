594d3bdef5ff0ef79cc6caa6dfe8a5e5c6ee2880
PercentBoat4164 <thadd.schelp@gmail.com>
2021-04-30 11:35:47 -0400

    finished writing rayTracingPipelineManager.hpp base.

--- src/GraphicsEngine/Vulkan/descriptorSetManager.hpp
+++ src/GraphicsEngine/Vulkan/descriptorSetManager.hpp
@@ -13,7 +13,7 @@
 struct DescriptorSetManagerCreateInfo {
     std::vector<VkDescriptorPoolSize> poolSizes{};
     std::vector<VkShaderStageFlagBits> shaderStages{};
-    std::vector<const char *> filenames{};
+    std::vector<std::vector<char>> shaderData{};
     std::vector<AccelerationStructureManager> accelerationStructures{};
     std::vector<ImageManager> images{};
     std::vector<BufferManager> buffers{};
@@ -24,30 +24,31 @@ public:
     VkDescriptorPool descriptorPool{};
     VkDescriptorSet descriptorSet{};
     VkDescriptorSetLayout descriptorSetLayout{};
+    DescriptorSetManagerCreateInfo *createdWith{};
 
     void destroy() {
         for (const std::function<void()>& function : deletionQueue) { function(); }
         deletionQueue.clear();
     }
 
-    void create(VulkanGraphicsEngineLink *renderEngineLink, DescriptorSetManagerCreateInfo createInfo) {
+    void create(VulkanGraphicsEngineLink *renderEngineLink, DescriptorSetManagerCreateInfo *createInfo) {
         linkedRenderEngine = renderEngineLink;
         //Create descriptor layout from bindings
         std::vector<VkDescriptorSetLayoutBinding> descriptorSetLayoutBindings{};
-        descriptorSetLayoutBindings.reserve(createInfo.poolSizes.size());
-        for (unsigned long i = 0; i < createInfo.poolSizes.size(); ++i) {
+        descriptorSetLayoutBindings.reserve(createdWith->poolSizes.size());
+        for (unsigned long i = 0; i < createdWith->poolSizes.size(); ++i) {
             VkDescriptorSetLayoutBinding descriptorSetLayoutBinding{};
-            descriptorSetLayoutBinding.descriptorType = createInfo.poolSizes[i].type;
+            descriptorSetLayoutBinding.descriptorType = createdWith->poolSizes[i].type;
             descriptorSetLayoutBinding.binding = i;
-            descriptorSetLayoutBinding.stageFlags = createInfo.shaderStages[i];
+            descriptorSetLayoutBinding.stageFlags = createdWith->shaderStages[i];
             descriptorSetLayoutBinding.descriptorCount = 1;
             descriptorSetLayoutBindings.push_back(descriptorSetLayoutBinding);
         }
         VkDescriptorSetLayoutCreateInfo descriptorSetLayoutCreateInfo{};
         if (vkCreateDescriptorSetLayout(linkedRenderEngine->device->device, &descriptorSetLayoutCreateInfo, nullptr, &descriptorSetLayout) != VK_SUCCESS) { throw std::runtime_error("failed to create descriptor layout!"); }
         VkDescriptorPoolCreateInfo descriptorPoolCreateInfo{VK_STRUCTURE_TYPE_DESCRIPTOR_POOL_CREATE_INFO};
-        descriptorPoolCreateInfo.poolSizeCount = static_cast<uint32_t>(createInfo.poolSizes.size());
-        descriptorPoolCreateInfo.pPoolSizes = createInfo.poolSizes.data();
+        descriptorPoolCreateInfo.poolSizeCount = static_cast<uint32_t>(createdWith->poolSizes.size());
+        descriptorPoolCreateInfo.pPoolSizes = createdWith->poolSizes.data();
         descriptorPoolCreateInfo.maxSets = 1;
         if (vkCreateDescriptorPool(linkedRenderEngine->device->device, &descriptorPoolCreateInfo, nullptr, &descriptorPool) != VK_SUCCESS) { throw std::runtime_error("failed to create descriptor pool!"); }
         deletionQueue.emplace_front([&]{ vkDestroyDescriptorPool(linkedRenderEngine->device->device, descriptorPool, nullptr); descriptorPool = VK_NULL_HANDLE; });
@@ -57,41 +58,41 @@ public:
         descriptorSetAllocateInfo.descriptorSetCount = 1;
         if (vkAllocateDescriptorSets(linkedRenderEngine->device->device, &descriptorSetAllocateInfo, &descriptorSet) != VK_SUCCESS) { throw std::runtime_error("failed to create descriptor set!"); }
         std::vector<VkWriteDescriptorSet> descriptorWrites{};
-        descriptorWrites.reserve(createInfo.poolSizes.size());
-        for (unsigned long i = 0; i < createInfo.poolSizes.size(); ++i) {
+        descriptorWrites.reserve(createdWith->poolSizes.size());
+        for (unsigned long i = 0; i < createdWith->poolSizes.size(); ++i) {
             VkWriteDescriptorSet writeDescriptorSet{VK_STRUCTURE_TYPE_WRITE_DESCRIPTOR_SET};
             writeDescriptorSet.dstSet = descriptorSet;
-            writeDescriptorSet.descriptorType = createInfo.poolSizes[i].type;
+            writeDescriptorSet.descriptorType = createdWith->poolSizes[i].type;
             writeDescriptorSet.dstBinding = i;
             if (writeDescriptorSet.descriptorType == VK_DESCRIPTOR_TYPE_ACCELERATION_STRUCTURE_KHR) {
                 VkWriteDescriptorSetAccelerationStructureKHR writeDescriptorSetAccelerationStructure{VK_STRUCTURE_TYPE_WRITE_DESCRIPTOR_SET_ACCELERATION_STRUCTURE_KHR};
                 writeDescriptorSetAccelerationStructure.accelerationStructureCount = 1;
-                writeDescriptorSetAccelerationStructure.pAccelerationStructures = &createInfo.accelerationStructures[i].accelerationStructure;
+                writeDescriptorSetAccelerationStructure.pAccelerationStructures = &createdWith->accelerationStructures[i].accelerationStructure;
                 writeDescriptorSet.pNext = &writeDescriptorSetAccelerationStructure;
             } else if (writeDescriptorSet.descriptorType == VK_DESCRIPTOR_TYPE_STORAGE_IMAGE) {
                 VkDescriptorImageInfo storageImageDescriptorInfo{};
-                storageImageDescriptorInfo.imageView = createInfo.images[i].view;
-                storageImageDescriptorInfo.sampler = createInfo.images[i].sampler;
-                storageImageDescriptorInfo.imageLayout = createInfo.images[i].imageLayout;
+                storageImageDescriptorInfo.imageView = createdWith->images[i].view;
+                storageImageDescriptorInfo.sampler = createdWith->images[i].sampler;
+                storageImageDescriptorInfo.imageLayout = createdWith->images[i].imageLayout;
                 if (storageImageDescriptorInfo.imageView == VK_NULL_HANDLE) { throw std::runtime_error("given image does not have an associated view!"); }
                 writeDescriptorSet.pImageInfo = &storageImageDescriptorInfo;
             } else if (writeDescriptorSet.descriptorType == VK_DESCRIPTOR_TYPE_COMBINED_IMAGE_SAMPLER) {
                 VkDescriptorImageInfo combinedImageSamplerDescriptorInfo{};
-                combinedImageSamplerDescriptorInfo.imageView = createInfo.images[i].view;
-                combinedImageSamplerDescriptorInfo.sampler = createInfo.images[i].sampler;
-                combinedImageSamplerDescriptorInfo.imageLayout = createInfo.images[i].imageLayout;
+                combinedImageSamplerDescriptorInfo.imageView = createdWith->images[i].view;
+                combinedImageSamplerDescriptorInfo.sampler = createdWith->images[i].sampler;
+                combinedImageSamplerDescriptorInfo.imageLayout = createdWith->images[i].imageLayout;
                 if (combinedImageSamplerDescriptorInfo.sampler == VK_NULL_HANDLE) { throw std::runtime_error("given image does not have an associated sampler!"); }
                 writeDescriptorSet.pImageInfo = &combinedImageSamplerDescriptorInfo;
             } else if (writeDescriptorSet.descriptorType == VK_DESCRIPTOR_TYPE_STORAGE_BUFFER) {
                 VkDescriptorBufferInfo storageBufferDescriptorInfo{};
-                storageBufferDescriptorInfo.buffer = createInfo.buffers[i].buffer;
+                storageBufferDescriptorInfo.buffer = createdWith->buffers[i].buffer;
                 storageBufferDescriptorInfo.offset = 0;
                 storageBufferDescriptorInfo.range = VK_WHOLE_SIZE;
                 if (storageBufferDescriptorInfo.buffer == VK_NULL_HANDLE) { throw std::runtime_error("given buffer has not been created!"); }
                 writeDescriptorSet.pBufferInfo = &storageBufferDescriptorInfo;
             } else if (writeDescriptorSet.descriptorType == VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER) {
                 VkDescriptorBufferInfo storageBufferDescriptorInfo{};
-                storageBufferDescriptorInfo.buffer = createInfo.buffers[i].buffer;
+                storageBufferDescriptorInfo.buffer = createdWith->buffers[i].buffer;
                 storageBufferDescriptorInfo.offset = 0;
                 storageBufferDescriptorInfo.range = VK_WHOLE_SIZE;
                 if (storageBufferDescriptorInfo.buffer == VK_NULL_HANDLE) { throw std::runtime_error("given buffer has not been created!"); }
--- src/GraphicsEngine/Vulkan/rayTracingPipelineManager.hpp
+++ src/GraphicsEngine/Vulkan/rayTracingPipelineManager.hpp
@@ -9,22 +9,68 @@ class RayTracingPipelineManager {
     VkPipelineLayout pipelineLayout{};
     DescriptorSetManager descriptorSetManager{};
     VulkanGraphicsEngineLink *linkedRenderEngine{};
-    std::vector<VkPipelineShaderStageCreateInfo> shaderStages{};
+    std::vector<VkRayTracingShaderGroupCreateInfoKHR> shaderGroups{};
+    VkPipeline pipeline{};
 
     void destroy() {
         for (const std::function<void()>& function : deletionQueue) { function(); }
         deletionQueue.clear();
     }
 
-    void create(const DescriptorSetManagerCreateInfo& descriptorSetManagerCreateInfo) {
+    void create(VulkanGraphicsEngineLink *renderEngineLink, DescriptorSetManagerCreateInfo *descriptorSetManagerCreateInfo) {
+        linkedRenderEngine = renderEngineLink;
         descriptorSetManager.create(linkedRenderEngine, descriptorSetManagerCreateInfo);
         VkPipelineLayoutCreateInfo pipelineLayoutCreateInfo{VK_STRUCTURE_TYPE_PIPELINE_LAYOUT_CREATE_INFO};
         pipelineLayoutCreateInfo.setLayoutCount = 1;
         pipelineLayoutCreateInfo.pSetLayouts = &descriptorSetManager.descriptorSetLayout;
         if (vkCreatePipelineLayout(linkedRenderEngine->device->device, &pipelineLayoutCreateInfo, nullptr, &pipelineLayout) != VK_SUCCESS) { throw std::runtime_error("failed to create pipeline layout!"); }
         deletionQueue.emplace_front([&] { vkDestroyPipelineLayout(linkedRenderEngine->device->device, pipelineLayout, nullptr); });
-//        shaderStages.reserve(descriptorSetManager)
-//        for ()
+        std::vector<VkPipelineShaderStageCreateInfo> shaderStages{};
+        shaderStages.reserve(descriptorSetManager.createdWith->shaderStages.size());
+        shaderGroups.reserve(descriptorSetManager.createdWith->shaderStages.size());
+        for (unsigned int i = 0; i > descriptorSetManager.createdWith->shaderStages.size(); ++i) {
+            VkShaderModule shaderModule;
+            VkShaderModuleCreateInfo shaderModuleCreateInfo{VK_STRUCTURE_TYPE_SHADER_MODULE_CREATE_INFO};
+            shaderModuleCreateInfo.codeSize = descriptorSetManager.createdWith->shaderData[i].size();
+            shaderModuleCreateInfo.pCode = reinterpret_cast<const uint32_t *>(descriptorSetManager.createdWith->shaderData[i].data());
+            if (vkCreateShaderModule(linkedRenderEngine->device->device, &shaderModuleCreateInfo, nullptr, &shaderModule) != VK_SUCCESS) {
+                for (VkPipelineShaderStageCreateInfo shader : shaderStages) { vkDestroyShaderModule(linkedRenderEngine->device->device, shader.module, nullptr); }
+                throw std::runtime_error("failed to create shader module!");
+            }
+            VkPipelineShaderStageCreateInfo pipelineShaderStageCreateInfo{VK_STRUCTURE_TYPE_PIPELINE_SHADER_STAGE_CREATE_INFO};
+            pipelineShaderStageCreateInfo.stage = descriptorSetManager.createdWith->shaderStages[i];
+            pipelineShaderStageCreateInfo.module = shaderModule;
+            pipelineShaderStageCreateInfo.pName = "main";
+            shaderStages.push_back(pipelineShaderStageCreateInfo);
+            VkRayTracingShaderGroupCreateInfoKHR rayTracingShaderGroupCreateInfo{VK_STRUCTURE_TYPE_RAY_TRACING_SHADER_GROUP_CREATE_INFO_KHR};
+            rayTracingShaderGroupCreateInfo.generalShader = VK_SHADER_UNUSED_KHR;
+            rayTracingShaderGroupCreateInfo.closestHitShader = VK_SHADER_UNUSED_KHR;
+            rayTracingShaderGroupCreateInfo.anyHitShader = VK_SHADER_UNUSED_KHR;
+            rayTracingShaderGroupCreateInfo.intersectionShader = VK_SHADER_UNUSED_KHR;
+            if (pipelineShaderStageCreateInfo.stage == VK_SHADER_STAGE_RAYGEN_BIT_KHR) {
+                rayTracingShaderGroupCreateInfo.type = VK_RAY_TRACING_SHADER_GROUP_TYPE_GENERAL_KHR;
+                rayTracingShaderGroupCreateInfo.generalShader = static_cast<uint32_t>(shaderStages.size()) - 1;
+            } else if (pipelineShaderStageCreateInfo.stage == VK_SHADER_STAGE_MISS_BIT_KHR) {
+                rayTracingShaderGroupCreateInfo.type = VK_RAY_TRACING_SHADER_GROUP_TYPE_GENERAL_KHR;
+                rayTracingShaderGroupCreateInfo.generalShader = static_cast<uint32_t>(shaderStages.size()) - 1;
+            } else if (pipelineShaderStageCreateInfo.stage == VK_SHADER_STAGE_CLOSEST_HIT_BIT_KHR) {
+                rayTracingShaderGroupCreateInfo.type = VK_RAY_TRACING_SHADER_GROUP_TYPE_TRIANGLES_HIT_GROUP_KHR;
+                rayTracingShaderGroupCreateInfo.closestHitShader = static_cast<uint32_t>(shaderStages.size()) - 1;
+            } else if (pipelineShaderStageCreateInfo.stage == VK_SHADER_STAGE_CALLABLE_BIT_KHR) {
+                rayTracingShaderGroupCreateInfo.type = VK_RAY_TRACING_SHADER_GROUP_TYPE_GENERAL_KHR;
+                rayTracingShaderGroupCreateInfo.generalShader = static_cast<uint32_t>(shaderStages.size()) - 1;
+            }
+            shaderGroups.push_back(rayTracingShaderGroupCreateInfo);
+        }
+        VkRayTracingPipelineCreateInfoKHR rayTracingPipelineCreateInfo{VK_STRUCTURE_TYPE_RAY_TRACING_PIPELINE_CREATE_INFO_KHR};
+        rayTracingPipelineCreateInfo.stageCount = static_cast<uint32_t>(shaderStages.size());
+        rayTracingPipelineCreateInfo.pStages = shaderStages.data();
+        rayTracingPipelineCreateInfo.groupCount = static_cast<uint32_t>(shaderGroups.size());
+        rayTracingPipelineCreateInfo.pGroups = shaderGroups.data();
+        rayTracingPipelineCreateInfo.maxPipelineRayRecursionDepth = 2;
+        rayTracingPipelineCreateInfo.layout = pipelineLayout;
+        vkCreateRayTracingPipelinesKHR(linkedRenderEngine->device->device, VK_NULL_HANDLE, VK_NULL_HANDLE, 1, &rayTracingPipelineCreateInfo, nullptr, &pipeline);
+        for (VkPipelineShaderStageCreateInfo shader : shaderStages) { vkDestroyShaderModule(linkedRenderEngine->device->device, shader.module, nullptr); }
     }
 
 private:
