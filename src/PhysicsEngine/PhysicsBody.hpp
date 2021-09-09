f21645bec9edc6ddb8656a5191e702c63421be0b
ethaninfinity <ethaninfinity@gmail.com>
2021-09-09 17:57:12 -0400

    Structure improvements and rotation framework

--- src/PhysicsEngine/PhysicsBody.hpp
+++ src/PhysicsEngine/PhysicsBody.hpp
@@ -1,7 +1,11 @@
 #pragma once
 
+#ifndef GLM_ENABLE_EXPERIMENTAL
+#define GLM_ENABLE_EXPERIMENTAL
+#endif
 #include "PhysicsMesh.hpp"
 #include <glm/glm.hpp>
+#include <glm/gtx/quaternion.hpp>
 
 //A physics body
 class PhysicsBody {
@@ -9,16 +13,46 @@ public:
 
     float mass{0}; //mass of the body
     glm::vec3 position{glm::vec3(0,0,0)}; //the position (and center of mass) of the body
+    glm::quat rotation{1,0,0,0}; //the current heading of the body
+    glm::quat rotationalVelocity{1,0,0,0}; //the rotational velocity
     glm::vec3 velocity{glm::vec3(0,0,0)}; //the velocity of the body
-    PhysicsMesh mesh{};
     
     //step the body forward in time
     void step(float time) {
+
+        //update xyz position 
         position += velocity;
+
+        rotation += glm::pow(rotationalVelocity, time);
     }
 
     //apply an impulse to accelerate the body
     void applyImpulse(glm::vec3 impulse) {
         velocity += impulse;
     }
+
+    //get the current location of a vertex in the mesh
+    glm::vec3 getMeshVertex(int index) {
+        return position + glm::rotate(rotation, mesh.vertices[index]);
+    }
+
+    std::vector<glm::vec3> getMeshTri(int index) {
+
+        //the triangle to be returned
+        std::vector<glm::vec3> tri = mesh.getTri(index);
+        
+        //move each vec3 in the tri to its current location in the physics world
+        for(int i = 0; i < 3; i++) {
+            tri[i] = glm::rotate(rotation, tri[i]);
+            tri[i] += position;
+        }
+        return tri;
+    }
+
+    int getMeshSize() {
+        return mesh.indices.size();
+    }
+
+private:
+    PhysicsMesh mesh{};
 };
--- src/PhysicsEngine/PhysicsWorld.hpp
+++ src/PhysicsEngine/PhysicsWorld.hpp
@@ -10,31 +10,60 @@ public:
     std::vector<PhysicsBody> bodies; //an array storing all bodies in the world
 
     //step in time
-    void step(float stepTime) {
-        float currTime = 0;
+    std::vector<PhysicsCollision> step(float stepTime) {
+        
+        float currTime = 0; //the time that has passed since the start of the function
+
+        std::vector<PhysicsCollision> allCollisions; //All collisions that have occured. Returned at the end of the function
+
         while(currTime < stepTime) {
             
-            //find the time of the earliest collision. If none is found, use stepTime.
-            float collisionTime = stepTime;
-            //@todo actually check for collisions
+            //Find the first upcoming collision in the world.
+            PhysicsCollision earliestCollision; //the first detected collision
+            PhysicsCollision currentCollision; //the collision currently being tested
+            
+            earliestCollision.time = stepTime - currTime;
+
+            //check collisions between every pair of bodies
+            //@TODO: manage two collisions that happen at the same time
+            for(int body1Index = 0; body1Index < bodies.size(); body1Index++) {
+                for(int body2Index = body1Index + 1; body2Index < bodies.size(); body2Index++) {
+                    currentCollision = checkCollision(bodies[body1Index], bodies[body2Index]);
+                    if(currentCollision.time <= earliestCollision.time) {
+                        earliestCollision = currentCollision;
+                    }
+                }
+            }
+
+            //move currTime forward to the time of the first collision
+            currTime += earliestCollision.time;
 
             //move to all bodies to where they will be at the time of the first collision
             for(int i = 0; i < bodies.size(); i++) {
 
                 //step forward from currTime to collisionTime
-                bodies[i].step(collisionTime - (stepTime - currTime)); 
+                bodies[i].step(earliestCollision.time); 
+            }
+
+            //add the earliest collision to the std::vector of collisions to be returned
+            if(earliestCollision.time != stepTime - currTime) {
+                earliestCollision.time = currTime;
+                allCollisions.push_back(earliestCollision);
             }
             
             //respond to first collision
         }
+        return allCollisions;
     }
 
     //find the collision time and location between objects. If negative, there is no collision.
     PhysicsCollision checkCollision(PhysicsBody body1, PhysicsBody body2) {
         
         PhysicsCollision collision; //the collision to return
-        collision.body1 = body1;
-        collision.body2 = body2;
+        collision.body1 = body1; //one body in the collision
+        collision.body2 = body2; //the other body in the collision
+        glm::vec3 body1InitialVelocity = body1.velocity; //the velocity of the first body before changing reference frames
+        glm::vec3 body2InitialVelocity = body2.velocity; //the velocity of the second body without changing reference frames
 
         std::vector<glm::vec3> testTri; //the tri currently being tested
         float testIntersectTime; //the intersection time of the current vertex and tri
@@ -43,27 +72,34 @@ public:
         // check each vertex on body1 against each tri on body2, then swap body1 and body2 and do it again
         for(int done = 0; done < 1; done++) {
             
+            //change the reference frame to that of the first body
+            body1.velocity += body2InitialVelocity;
+            body2.velocity = glm::vec3(0,0,0);
+
             //swap body1 and body2 after the first iteration
             if(done = 1) {
                 PhysicsBody swap = body1;
                 body1 = body2;
                 body2 = swap;
+                //change the reference frame to that of the first body
+                body1.velocity += body2InitialVelocity;
+                body2.velocity = glm::vec3(0,0,0);
             }
 
             //iterate through body1's vertices and body2's tris
-            for(int body1Vertex = 0; body1Vertex < body1.mesh.indices.size(); body1Vertex++) {
-                for(int body2Index = 0; body2Index < body2.mesh.indices.size(); body2Index += 3) {
+            for(int body1Vertex = 0; body1Vertex < body1.getMeshSize(); body1Vertex++) {
+                for(int body2Index = 0; body2Index < body2.getMeshSize(); body2Index += 3) {
                     
                     //get the normal vector of the current tri's plane
-                    planeNormal = glm::normalize(glm::cross(body2.mesh.vertices[body2Index], body2.mesh.vertices[body2Index + 1]));
+                    planeNormal = glm::normalize(glm::cross(body2.getMeshVertex(body2Index), body2.getMeshVertex(body2Index + 1)));
                     
                     //get the time of collision
-                    testIntersectTime = getVertexPlaneIntersectTime(body1.mesh.vertices[body1Vertex], body1.velocity, (body2.mesh.getTri(body2Index))[0], planeNormal);
+                    testIntersectTime = getVertexPlaneIntersectTime(body1.getMeshVertex(body1Vertex), body1.velocity, (body2.getMeshTri(body2Index))[0], planeNormal);
                     
                     //if the collision happens and is the soonest one yet, make it the new first collision
                     if(collision.time < testIntersectTime && collision.time > 0) {
-                        testTri = body2.mesh.getTri(body2Index);
-                        if(isPointInTriangle(body1.mesh.vertices[body1Vertex] + (body1.velocity * testIntersectTime), testTri)) {
+                        testTri = body2.getMeshTri(body2Index);
+                        if(isPointInTriangle(body1.getMeshVertex(body1Vertex) + (body1.velocity * testIntersectTime), testTri)) {
                             collision.time = testIntersectTime;
                             collision.body1Vertex = body1Vertex;
                             collision.body2TriIndex = body2Index;
