e23e1075290ba4dec93e46c92484d935d8324d77
Thaddeus Schelp <thadd.schelp@gmail.com>
2021-11-07 15:23:54 -0500

    Added SyntaxGuidelines.cpp. Did not change files to follow them yet.

--- src/GraphicsModule/IEGraphicsModuleMain.cpp
+++ src/GraphicsModule/IEGraphicsModuleMain.cpp
@@ -6,29 +6,11 @@
 
 #include "Vulkan/vulkanRenderEngine.hpp"
 #include "OpenGL/openglRenderEngine.hpp"
-#include "Merged/IeRenderEngine.hpp"
-#include "LogModule/Log.hpp"
 
 /*
  * Any code for testing the entire graphics module goes here.
  */
 int main(int argc, char **argv) {
-    std::string selection;
-    std::string input;
-    bool force{false};
-    if (argc > 1) {
-        if (*argv[1] == 'v') { input = "Vulkan"; }
-        if (*argv[1] == 'o') { input = "OpenGL"; }
-        force = *(argv[1] + 1) == 'f';
-    } if (input.empty()) {
-        std::cout << "'v': Run Vulkan render engine\n'o': Run OpenGL render engine\n";
-        std::cin >> selection;
-        if (selection[0] == 'v') { input = "Vulkan"; }
-        if (selection[0] == 'o') { input = "OpenGL"; }
-    } if (input == "Vulkan" & !glfwVulkanSupported() & !force) {
-        input = "OpenGL";
-        std::cout << "Forcing OpenGL because Vulkan is not supported" << std::endl;
-    }
     OpenGLRenderEngine oRenderEngine{};
     OpenGLRenderable oCube{&oRenderEngine.renderEngineLink, "res/Models/Cube/cube.obj"};
     oRenderEngine.loadRenderable(&oCube);
@@ -45,11 +27,4 @@ int main(int argc, char **argv) {
         glfwPollEvents();
     }
     vRenderEngine.destroy();
-    Log log{};
-    IeRenderEngine mRenderEngine{input, &log};
-    mRenderEngine.create();
-    while (oRenderEngine.update()) {
-        glfwPollEvents();
-    }
-    mRenderEngine.destroy();
 }
