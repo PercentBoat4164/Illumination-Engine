6662b9d13be78b59ccbf2ca4f9daa727fd826cbe
PercentBoat4164 <thadd.schelp@gmail.com>
2021-11-12 00:46:42 -0500

    All basic tests working.

--- src/GraphicsModule/Vulkan/IEGMVulkanMain.cpp
+++ src/GraphicsModule/Vulkan/IEGMVulkanMain.cpp
@@ -6,7 +6,21 @@
  * Any code for testing Vulkan goes here.
  */
 int main(int argc, char **argv) {
-    if (!glfwVulkanSupported() & (*(argv[1]) != 'f')) { throw std::runtime_error("Vulkan is not supported on this device."); } else { std::cerr << "Attempting to run Vulkan on unsupported device!" << std::endl; }
+    bool force{false};
+    if (argc > 1) {
+        if (*(argv[1]) == 'f') {
+            force = true;
+        }
+    }
+    glfwInit();
+    if (!glfwVulkanSupported()) {
+        if (!force) {
+            throw std::runtime_error("Vulkan is not supported on this device.");
+        }
+        else {
+            std::cerr << "Attempting to run Vulkan on unsupported device!" << std::endl;
+        }
+    }
     VulkanRenderEngine renderEngine{};
     VulkanRenderable cube{&renderEngine.renderEngineLink, "res/Models/Cube/cube.obj"};
     cube.position = {0, -3, 0};
