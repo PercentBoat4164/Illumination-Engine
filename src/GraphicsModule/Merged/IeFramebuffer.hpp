1446a40521a7984cd9ce04acb5614102ca4fbfe6
Thaddeus Schelp <thadd.schelp@gmail.com>
2021-11-14 00:18:53 -0500

    Progress save. Works!

--- src/GraphicsModule/Merged/IeFramebuffer.hpp
+++ src/GraphicsModule/Merged/IeFramebuffer.hpp
@@ -19,9 +19,9 @@ enum IeFramebufferAttachmentFormat {
 };
 
 struct IeRenderPassAttachmentDescription {
-    VkAttachmentDescription color;
     VkAttachmentDescription depth;
-    VkAttachmentDescription resolve;
+    std::vector<VkAttachmentDescription> color;
+    std::vector<VkAttachmentDescription> resolve;
 };
 
 class IeRenderPass;
@@ -36,6 +36,7 @@ public:
         IeFramebuffer* dependentOn;
         IeFramebuffer* requiredBy;
         uint8_t subpass{0};
+        uint32_t colorImageCount{1};
     };
 
     struct Created {
@@ -66,43 +67,54 @@ public:
      * @brief Generates the necessary data to link this framebuffers to a renderpass.
      * @param createInfo
      */
-    static IeRenderPassAttachmentDescription generateAttachmentDescriptions(IeFramebuffer::CreateInfo createInfo) {
+    static IeRenderPassAttachmentDescription generateAttachmentDescriptions(IeRenderEngineLink* linkedRenderEngine, IeFramebuffer::CreateInfo* createInfo) {
         IeRenderPassAttachmentDescription renderPassAttachmentDescription{}; // prepare result data
-        if (createInfo.aspects & IE_FRAMEBUFFER_ASPECT_COLOR_BIT) {
-            auto format = ieImageFormats.find(createInfo.format);
-            if (createInfo.msaaSamples <= 1) {
-                renderPassAttachmentDescription.color = VkAttachmentDescription{
-                        .format=format->second.first,
-                        .samples=static_cast<VkSampleCountFlagBits>(createInfo.msaaSamples),
-                        .loadOp=createInfo.dependentOn ? VK_ATTACHMENT_LOAD_OP_LOAD : VK_ATTACHMENT_LOAD_OP_CLEAR,
-                        .storeOp=createInfo.requiredBy ? VK_ATTACHMENT_STORE_OP_STORE : VK_ATTACHMENT_STORE_OP_DONT_CARE,
-                        .stencilLoadOp=createInfo.dependentOn ? VK_ATTACHMENT_LOAD_OP_LOAD : VK_ATTACHMENT_LOAD_OP_CLEAR,
-                        .stencilStoreOp=createInfo.requiredBy ? VK_ATTACHMENT_STORE_OP_STORE : VK_ATTACHMENT_STORE_OP_DONT_CARE,
-                        .initialLayout=VK_IMAGE_LAYOUT_UNDEFINED,
-                        .finalLayout=createInfo.requiredBy ? VK_IMAGE_LAYOUT_READ_ONLY_OPTIMAL_KHR : VK_IMAGE_LAYOUT_PRESENT_SRC_KHR
-                };
+        if (createInfo->aspects & IE_FRAMEBUFFER_ASPECT_COLOR_BIT) {
+            if (createInfo->colorImageCount == 0) {
+                linkedRenderEngine->log->log("Color bit set, but requested 0 color images. Creating one color image anyway.", log4cplus::WARN_LOG_LEVEL, "Graphics Module");
             }
-            else {
-                renderPassAttachmentDescription.resolve = VkAttachmentDescription{
-                        .format=format->second.first,
-                        .samples=VK_SAMPLE_COUNT_1_BIT,
-                        .loadOp=createInfo.dependentOn ? VK_ATTACHMENT_LOAD_OP_LOAD : VK_ATTACHMENT_LOAD_OP_CLEAR,
-                        .storeOp=createInfo.requiredBy ? VK_ATTACHMENT_STORE_OP_STORE : VK_ATTACHMENT_STORE_OP_DONT_CARE,
-                        .stencilLoadOp=createInfo.dependentOn ? VK_ATTACHMENT_LOAD_OP_LOAD : VK_ATTACHMENT_LOAD_OP_CLEAR,
-                        .stencilStoreOp=createInfo.requiredBy ? VK_ATTACHMENT_STORE_OP_STORE : VK_ATTACHMENT_STORE_OP_DONT_CARE,
-                        .initialLayout=VK_IMAGE_LAYOUT_UNDEFINED,
-                        .finalLayout=VK_IMAGE_LAYOUT_PRESENT_SRC_KHR
+            createInfo->colorImageCount = std::max(1u, createInfo->colorImageCount);
+            auto format = ieImageFormats.find(createInfo->format);
+            if (format == ieImageFormats.end()) {
+                return renderPassAttachmentDescription;
+            }
+            renderPassAttachmentDescription.color = {
+                    createInfo->colorImageCount,
+                    VkAttachmentDescription{
+                            .format=format->second.first,
+                            .samples=static_cast<VkSampleCountFlagBits>(createInfo->msaaSamples),
+                            .loadOp=createInfo->dependentOn ? VK_ATTACHMENT_LOAD_OP_LOAD : VK_ATTACHMENT_LOAD_OP_CLEAR,
+                            .storeOp=createInfo->requiredBy ? VK_ATTACHMENT_STORE_OP_STORE : VK_ATTACHMENT_STORE_OP_DONT_CARE,
+                            .stencilLoadOp=createInfo->dependentOn ? VK_ATTACHMENT_LOAD_OP_LOAD : VK_ATTACHMENT_LOAD_OP_CLEAR,
+                            .stencilStoreOp=createInfo->requiredBy ? VK_ATTACHMENT_STORE_OP_STORE : VK_ATTACHMENT_STORE_OP_DONT_CARE,
+                            .initialLayout=VK_IMAGE_LAYOUT_UNDEFINED,
+                            .finalLayout=createInfo->requiredBy ? VK_IMAGE_LAYOUT_READ_ONLY_OPTIMAL_KHR : VK_IMAGE_LAYOUT_PRESENT_SRC_KHR
+                    }
+            };
+            if (createInfo->msaaSamples > 1) {
+                renderPassAttachmentDescription.resolve = {
+                        createInfo->colorImageCount,
+                        VkAttachmentDescription{
+                                .format=format->second.first,
+                                .samples=VK_SAMPLE_COUNT_1_BIT,
+                                .loadOp=createInfo->dependentOn ? VK_ATTACHMENT_LOAD_OP_LOAD : VK_ATTACHMENT_LOAD_OP_CLEAR,
+                                .storeOp=createInfo->requiredBy ? VK_ATTACHMENT_STORE_OP_STORE : VK_ATTACHMENT_STORE_OP_DONT_CARE,
+                                .stencilLoadOp=createInfo->dependentOn ? VK_ATTACHMENT_LOAD_OP_LOAD : VK_ATTACHMENT_LOAD_OP_CLEAR,
+                                .stencilStoreOp=createInfo->requiredBy ? VK_ATTACHMENT_STORE_OP_STORE : VK_ATTACHMENT_STORE_OP_DONT_CARE,
+                                .initialLayout=VK_IMAGE_LAYOUT_UNDEFINED,
+                                .finalLayout=VK_IMAGE_LAYOUT_PRESENT_SRC_KHR
+                        }
                 };
             }
         }
-        if (createInfo.aspects & IE_FRAMEBUFFER_ASPECT_DEPTH_BIT) {
+        if (createInfo->aspects & IE_FRAMEBUFFER_ASPECT_DEPTH_BIT) {
             renderPassAttachmentDescription.depth = VkAttachmentDescription{
                     .format=static_cast<VkFormat>(IE_FRAMEBUFFER_DEPTH_ATTACHMENT_FORMAT),
-                    .samples=static_cast<VkSampleCountFlagBits>(createInfo.msaaSamples),
-                    .loadOp=createInfo.dependentOn ? VK_ATTACHMENT_LOAD_OP_LOAD : VK_ATTACHMENT_LOAD_OP_CLEAR,
-                    .storeOp=createInfo.requiredBy ? VK_ATTACHMENT_STORE_OP_STORE : VK_ATTACHMENT_STORE_OP_DONT_CARE,
-                    .stencilLoadOp=createInfo.dependentOn ? VK_ATTACHMENT_LOAD_OP_LOAD : VK_ATTACHMENT_LOAD_OP_CLEAR,
-                    .stencilStoreOp=createInfo.requiredBy ? VK_ATTACHMENT_STORE_OP_STORE : VK_ATTACHMENT_STORE_OP_DONT_CARE,
+                    .samples=static_cast<VkSampleCountFlagBits>(createInfo->msaaSamples),
+                    .loadOp=createInfo->dependentOn ? VK_ATTACHMENT_LOAD_OP_LOAD : VK_ATTACHMENT_LOAD_OP_CLEAR,
+                    .storeOp=createInfo->requiredBy ? VK_ATTACHMENT_STORE_OP_STORE : VK_ATTACHMENT_STORE_OP_DONT_CARE,
+                    .stencilLoadOp=createInfo->dependentOn ? VK_ATTACHMENT_LOAD_OP_LOAD : VK_ATTACHMENT_LOAD_OP_CLEAR,
+                    .stencilStoreOp=createInfo->requiredBy ? VK_ATTACHMENT_STORE_OP_STORE : VK_ATTACHMENT_STORE_OP_DONT_CARE,
                     .initialLayout=VK_IMAGE_LAYOUT_UNDEFINED,
                     .finalLayout=VK_IMAGE_LAYOUT_DEPTH_STENCIL_ATTACHMENT_OPTIMAL
             };
--- src/GraphicsModule/Merged/IeRenderPass.hpp
+++ src/GraphicsModule/Merged/IeRenderPass.hpp
@@ -24,25 +24,50 @@ public:
                   [&](IeFramebuffer::CreateInfo first, IeFramebuffer::CreateInfo second) { return first.subpass > second.subpass; });
         attachmentDescriptions.clear();
         attachmentDescriptions.reserve(createdWith.attachments.size());
-        for (IeFramebuffer::CreateInfo attachment : createdWith.attachments) {
-            attachmentDescriptions.push_back(IeFramebuffer::generateAttachmentDescriptions(attachment));
+        for (IeFramebuffer::CreateInfo attachment : createdWith.attachments) { // @todo Eliminate need to sort.
+            attachmentDescriptions.push_back(IeFramebuffer::generateAttachmentDescriptions(linkedRenderEngine, &attachment));
         }
         subpasses.clear();
         subpasses.reserve(createdWith.attachments[createdWith.attachments.size() - 1].subpass + 1);
         VkAttachmentReference depthAttachmentReference;
         std::vector<VkAttachmentReference> colorAttachmentReferences;
         std::vector<VkAttachmentReference> resolveAttachmentReferences;
-        for (IeFramebuffer::CreateInfo attachment : createdWith.attachments) {
+        for (uint32_t i = 0; i < subpasses.capacity(); ++i) {
+            for (uint32_t j = 0; j < attachmentDescriptions.size(); ++j) {
+                if (createdWith.attachments[j].subpass == i) {
+                    depthAttachmentReference = {
+                            .attachment=j * 3 + 0,
+                            .layout=attachmentDescriptions[j].depth.finalLayout // @todo Fix dpeth not being initialized.
+                    };
+                    if (!attachmentDescriptions[j].color.empty()) {
+                        colorAttachmentReferences = {};
+                        colorAttachmentReferences.reserve(attachmentDescriptions[j].color.size());
+
+                        resolveAttachmentReferences = {};
+                        resolveAttachmentReferences.reserve(attachmentDescriptions[j].resolve.size());
+                    }
+                    for (uint32_t k = 0; k < attachmentDescriptions[j].color.size(); ++k) {
+                        colorAttachmentReferences.push_back({
+                                .attachment=j * 3 + 1,
+                                .layout=attachmentDescriptions[j].color[k].finalLayout
+                        });
+                        resolveAttachmentReferences.push_back({
+                                .attachment=j * 3 + 2,
+                                .layout=attachmentDescriptions[j].resolve[k].finalLayout
+                        });
+                    }
+                }
+            }
             subpasses.push_back(VkSubpassDescription{
                     .pipelineBindPoint=VK_PIPELINE_BIND_POINT_GRAPHICS,
                     .colorAttachmentCount=1,
                     .pColorAttachments=nullptr
             });
         }
-        VkRenderPassCreateInfo renderPassCreateInfo{};
-        if (vkCreateRenderPass(linkedRenderEngine->device.device, &renderPassCreateInfo, nullptr, &renderPass) != VK_SUCCESS) {
-            linkedRenderEngine->log->log("Failed to create render pass!", log4cplus::DEBUG_LOG_LEVEL, "Graphics Module");
-        }
+//        VkRenderPassCreateInfo renderPassCreateInfo{};
+//        if (vkCreateRenderPass(linkedRenderEngine->device.device, &renderPassCreateInfo, nullptr, &renderPass) != VK_SUCCESS) {
+//            linkedRenderEngine->log->log("Failed to create render pass!", log4cplus::DEBUG_LOG_LEVEL, "Graphics Module");
+//        }
     }
 
 private:
