dab35b878f310bae2298571c33f30ca40123806d
PercentBoat4164 <thadd.schelp@gmail.com>
2021-12-24 00:46:49 -0500

    Change of plans moving forward.

---
+++ src/GraphicsModule/VulkanOpenGL/IEGMVulkanOpenGLMain.cpp
@@ -0,0 +1,36 @@
+#ifdef ILLUMINATION_ENGINE_VULKAN
+#include "IERenderEngine.hpp"
+#endif
+
+#include <iostream>
+
+/*
+ * Any code for testing Vulkan goes here.
+ */
+int main(int argc, char **argv) {
+    #ifdef ILLUMINATION_ENGINE_VULKAN
+    bool force{false};
+    if (argc > 1) {
+        if (*(argv[1]) == 'f') {
+            force = true;
+        }
+    }
+    glfwInit();
+    if (!glfwVulkanSupported()) {
+        if (!force) {
+            throw std::runtime_error("Vulkan is not supported on this device.");
+        }
+        else {
+            std::cerr << "Attempting to run Vulkan on unsupported device!" << std::endl;
+        }
+    }
+    IERenderEngine renderEngine{};
+    IERenderable cube{&renderEngine.renderEngineLink, "res/Models/Cube/cube.obj"};
+    cube.position = {0, -3, 0};
+    renderEngine.loadRenderable(&cube);
+    while (renderEngine.update()) {
+        glfwPollEvents();
+    }
+    renderEngine.destroy();
+    #endif
+}
