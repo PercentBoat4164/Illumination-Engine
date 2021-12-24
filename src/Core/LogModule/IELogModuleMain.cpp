e3e61498f207caf02d155cc930e50fa5cc54a00b
PercentBoat4164 <thadd.schelp@gmail.com>
2021-12-24 01:19:05 -0500

    Update IELogger documentation.

--- src/Core/LogModule/IELogModuleMain.cpp
+++ src/Core/LogModule/IELogModuleMain.cpp
@@ -4,8 +4,9 @@
  * Any code for testing the log module goes here.
  */
 int main(int argc, char **argv) {
+    IELogger::logDefault(ILLUMINATION_ENGINE_LOG_LEVEL_INFO, "This line tests the static function call.");
     IELogger logger{"IEGM"};
-    IELogger::logDefault(ILLUMINATION_ENGINE_LOG_LEVEL_TRACE, "This line tests the static function call.");
-    logger.logToAll(ILLUMINATION_ENGINE_LOG_LEVEL_0, "This line tests the static function call from an object.");
-    logger(spdlog::level::trace, "This line tests the operator overload.");
+    IELogger::logDefault(ILLUMINATION_ENGINE_LOG_LEVEL_DEBUG, "This line tests higher level stuff.");
+    logger.logToAll(ILLUMINATION_ENGINE_LOG_LEVEL_INFO, "This line tests the static function call from an object.");
+    logger(ILLUMINATION_ENGINE_LOG_LEVEL_CRITICAL, "This line tests the operator overload.");
 }
