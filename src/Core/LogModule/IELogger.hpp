81413d815d0cf2ebd9507c0aa330a33f01b6665f
PercentBoat4164 <thadd.schelp@gmail.com>
2021-12-24 01:19:05 -0500

    Update IELogger documentation.

--- src/Core/LogModule/IELogger.hpp
+++ src/Core/LogModule/IELogger.hpp
@@ -3,6 +3,7 @@
 #include "spdlog/spdlog.h"
 #include "spdlog/sinks/basic_file_sink.h"
 
+//Illumination Engine replacements for spdlog log levels.
 #define ILLUMINATION_ENGINE_LOG_LEVEL_TRACE spdlog::level::trace
 #define ILLUMINATION_ENGINE_LOG_LEVEL_DEBUG spdlog::level::debug
 #define ILLUMINATION_ENGINE_LOG_LEVEL_INFO spdlog::level::info
@@ -53,6 +54,12 @@ public:
         spdlog::set_default_logger(loggers[0]);
     };
 
+    /**
+     * @brief Logs [message] at [level] to all loggers named [name].
+     * @param level
+     * @param message
+     * @param name
+     */
     void logToAll(spdlog::level::level_enum level, const std::string& message, const std::string& name= "") {
         for (const std::shared_ptr<spdlog::logger>& logger : loggers) {
             if (name.empty() || logger->name() == name) {
@@ -61,26 +68,49 @@ public:
         }
     }
 
+    /**
+     * @brief Adds a logger with name [name] to the internal list of loggers.
+     * @param name
+     */
     void addLog(const std::string& name) {
         loggers.push_back(spdlog::basic_logger_mt(name, "logs/" + name + ".log", true));
     }
 
+    /**
+     * @brief Adds a logger that outputs to [file] with name [name] to the internal list of loggers.
+     * @param name
+     */
     void addLog(const std::string& name, const std::string& file) {
         loggers.push_back(spdlog::basic_logger_mt(name, "logs/" + file + ".log", true));
     }
 
+    /**
+     * @brief Logs [message] at [level] to the current logger.
+     * @param level
+     * @param message
+     */
     static void logDefault(spdlog::level::level_enum level, const std::string& message) {
         spdlog::log(level, message);
     }
 
+    /**
+     * @brief Logs [message] at [level] to the current logger.
+     * @param level
+     * @param message
+     */
     void operator()(spdlog::level::level_enum level, const std::string& message) {
         logToAll(level, message);
     }
 
+    /**
+     * @brief Get the logger at [index].
+     * @param index
+     * @return Logger in internal list at [index].
+     */
     std::shared_ptr<spdlog::logger> operator[](uint32_t index) {
         return loggers[index];
     }
 
 private:
-    std::vector<std::shared_ptr<spdlog::logger>> loggers;
+    std::vector<std::shared_ptr<spdlog::logger>> loggers; // Internal list of loggers
 };
