48aec9ce40d42392b109c87f2efc007779f31718
ethaninfinity <ethaninfinity@gmail.com>
2022-07-05 18:32:12 -0400

    Added more powerful delete function

--- src/Core/FileSystemModule/IEFileSystem.hpp
+++ src/Core/FileSystemModule/IEFileSystem.hpp
@@ -14,8 +14,6 @@ class IEFileSystem {
 public:
     std::filesystem::path path;
     IEImporter importer;
-    std::unordered_map<std::string, IEFile> files;
-    std::unordered_map<std::string, IETempFile> tempFiles;
 
     //constructor
     IEFileSystem(std::filesystem::path fileSystemPath) {
@@ -51,8 +49,24 @@ public:
         }
     }
 
+    //delete a directory that has other files in it
+    void deleteUsedDirectory(std::string filePath) {
+        std::string testPath;
+        for(auto const& x : files) {
+            testPath = x.second.path.string().substr(0, filePath.size());
+            if(testPath == filePath) {
+                files.erase(x.first);
+            }
+        }
+        std::filesystem::remove_all(path / filePath);
+    }
+
     //import a file
     std::string importFile(std::string filePath) {
         return importer.import(files.find(filePath)->second);
     }
+
+private:
+    std::unordered_map<std::string, IEFile> files;
+    std::unordered_map<std::string, IETempFile> tempFiles;
 };
