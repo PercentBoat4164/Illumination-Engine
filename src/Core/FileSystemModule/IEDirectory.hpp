aac4d7489dc619880a4466366e8cef25fdb6c2c5
PercentBoat4164 <thadd.schelp@gmail.com>
2022-08-28 17:43:02 -0400

    - Full Windows / MSVC compatibility.

--- src/Core/FileSystemModule/IEDirectory.hpp
+++ src/Core/FileSystemModule/IEDirectory.hpp
@@ -3,8 +3,6 @@
 #include "IEFile.hpp"
 
 #include <vector>
-#include <functional>
-
 
 class IEDirectory {
 public:
@@ -17,49 +15,20 @@ public:
      * @brief Creates a new IEDirectory with a path
      * @param initialPath
      */
-    explicit IEDirectory(const std::string& initialPath) {
-        path = initialPath;
-    }
+    explicit IEDirectory(const std::string& initialPath);
 
     std::vector<IEDirectory*> subDirectories{};
     std::vector<IEFile*> files{};
     std::string path{};
     bool exists{};
 
-    void createDirectory() const {
-        for (int i = 0; i < path.size(); ++i) {
-            if (path[i] == '/') {
-                std::filesystem::create_directory(path.substr(0, i));
-            }
-        }
-        std::filesystem::create_directory(path);
-    }
+    void createDirectory() const;
 
-    std::vector<IEDirectory*> allDirectories() {
-        std::vector<IEDirectory*> allSubDirectories{subDirectories};
-        for (IEDirectory* subDirectory : subDirectories) {
-            std::vector<IEDirectory*> theseSubDirectories{subDirectory->allDirectories()};
-            allSubDirectories.insert(allSubDirectories.end(), theseSubDirectories.begin(), theseSubDirectories.end());
-        }
-        return allSubDirectories;
-    }
+    std::vector<IEDirectory*> allDirectories();
 
-    std::vector<IEFile*> allFiles() {
-        std::vector<IEFile*> allFiles{files};
-        for (IEDirectory* subDirectory : subDirectories) {
-            std::vector<IEFile*> theseFiles{subDirectory->allFiles()};
-            allFiles.insert(allFiles.end(), theseFiles.begin(), theseFiles.end());
-        }
-        return allFiles;
-    }
+    std::vector<IEFile*> allFiles();
 
-    void create() {
-        createDirectory();
-        exists = true;
-    }
+    void create();
 
-    void remove() {
-        std::filesystem::remove_all(path);
-        exists = false;
-    }
+    void remove();
 };
--- src/Core/FileSystemModule/IEFile.hpp
+++ src/Core/FileSystemModule/IEFile.hpp
@@ -1,7 +1,5 @@
 #pragma once
 
-#include "Core/LogModule/IELogger.hpp"
-
 #include <vector>
 #include <string>
 #include <filesystem>
@@ -13,161 +11,31 @@ class IEFile {
 public:
 	IEFile() = default;
 
-	explicit IEFile(const std::string &initialPath) {
-		// Record this filename
-		path = initialPath;
-	}
+	explicit IEFile(const std::string &initialPath);
 
 	std::streamsize length{};
 	std::string path{};
 	std::fstream file{};
 
-	std::string getDirectory() const {
-		return path.substr(0, path.find_last_of('/'));
-	}
+	std::string getDirectory() const;
 
-	void createDirectory() const {
-		std::string thisDirectory{getDirectory()};
-		for (size_t i = 0; i < thisDirectory.size(); ++i) {
-			if (thisDirectory[i] == '/') {
-				std::filesystem::create_directory(thisDirectory.substr(0, i));
-			}
-		}
-		std::filesystem::create_directory(thisDirectory);
-	}
+	void createDirectory() const;
 
-	bool open() {
-		if (file.is_open()) {
-			return true;
-		}
-		file.open(path, std::ios_base::in | std::ios_base::out | std::ios_base::ate | std::ios_base::binary);
-		if (!file.is_open()) {
-			file.open(path, std::ios_base::in | std::ios_base::out | std::ios_base::binary);
-		}
-		if (!file.is_open()) {
-			createDirectory();
-			file.open(path, std::ios_base::in | std::ios_base::out | std::ios_base::binary);
-		}
-		if (file.is_open()) {
-			length = file.tellg();
-			return true;
-		}
-		return false;
-	}
+	bool open();
 
-	void close() {
-		if (!file.is_open()) {
-			return;
-		}
-		file.close();
-	}
+	void close();
 
-	virtual std::string read(size_t numBytes, std::streamsize startPosition = -1) {
-		if (startPosition == -1) {  // If no starting position
-			startPosition = file.tellg();  // Start here
-		}
-		// Go to start position
-		file.seekg(startPosition);
+	virtual std::string read(size_t numBytes, std::streamsize startPosition = -1);
 
-		// Create space for contents
-		std::vector<char> bytes{};
-		bytes.resize(numBytes);
+	void insert(const std::string &data, std::streamsize startPosition = -1);
 
-		// Read the specified number of bytes into the char array
-		file.read(bytes.data(), numBytes);
+	void overwrite(const std::string &data, std::streamsize startPosition = -1);
 
-		// Convert to string
-		return std::string{bytes.data(), bytes.size()};
-	}
+	void erase(std::streamsize numBytes, std::streamsize startPosition = -1);
 
-	void insert(const std::string &data, std::streamsize startPosition = -1) {
-		if (startPosition > length) {
-			// Attempt to insert from beyond the length of the file!
-			return;
-		}
-		if (startPosition == -1) {  // If no starting position
-			startPosition = file.tellg();  // Start here
-		}
+	std::vector<std::string> extensions() const;
 
-		// Read data that is about to be overwritten
-		std::string bytes = read((size_t) (length - startPosition), startPosition);
+	~IEFile();
 
-		// Go to starting position
-		file.seekg(startPosition);
-
-
-		// Write entirety of data and file contents after data
-		overwrite(data + bytes, startPosition);
-
-		// Update file length
-		auto dataSize = static_cast<std::streamsize>(startPosition + data.size());
-		length = static_cast<std::streamsize>(dataSize + bytes.size());
-
-		// Go to end of new data
-		file.seekg(dataSize);
-	}
-
-	void overwrite(const std::string &data, std::streamsize startPosition = -1) {
-		if (startPosition == -1) {  // If no starting position
-			startPosition = file.tellg();  // Start here
-		}
-
-		// Go to starting position
-		file.seekg(startPosition);
-
-		// Write to file
-		auto dataSize = static_cast<std::streamsize>(data.size());
-		file.write(data.data(), dataSize);
-
-		// Update file length
-		length = std::max(dataSize + startPosition, length);
-	}
-
-	void erase(std::streamsize numBytes, std::streamsize startPosition = -1) {
-		if (startPosition == -1) {  // If no starting position
-			startPosition = file.tellg();  // Start here
-		}
-
-		// Go to where file should stop being erased
-		file.seekg(startPosition + numBytes);
-
-		// Read to EOF into a buffer
-		std::string bytes{read((size_t) (length - file.tellg()))};
-
-		// Resize file to the size of only the data to be kept
-		std::filesystem::resize_file(path, startPosition);
-
-		// Write the read data back to the file
-		length = 0;  // Set length to zero so that it will be updated properly in the overwrite function
-		overwrite(bytes, startPosition);
-	}
-
-	std::vector<std::string> extensions() const {
-		std::string temporaryPath = path.substr(path.find_last_of('/') + 1);
-		std::vector<std::string> result;
-		std::regex rgx("[.]");
-		std::sregex_token_iterator iter(temporaryPath.begin(), temporaryPath.end(), rgx, -1);
-		std::sregex_token_iterator end;
-		for (++iter; iter != end; ++iter) {
-			result.push_back(*iter);
-		}
-		return result;
-	}
-
-	~IEFile() {
-		close();
-	}
-
-	IEFile &operator=(const IEFile &other) {
-		// Copy over data from other
-		path = other.path;
-		length = other.length;
-
-		// Close this file to avoid memory leaks
-		close();
-
-		// Open the new file
-		open();
-		return *this;
-	}
+	IEFile &operator=(const IEFile &other);
 };
