53e627e7273b8d775d27a28500322250ac4c52f9
PercentBoat4164 <thadd.schelp@gmail.com>
2022-11-22 15:12:44 -0500

    - Added the FBX renderable. - Applied formatting.

--- src/Core/Core.hpp
+++ src/Core/Core.hpp
@@ -20,7 +20,7 @@ public:
     static IE::Core::Core &getInst(const std::filesystem::path &t_path = "");
 
     template<typename T, typename... Args>
-    requires std::derived_from<T, IE::Core::Engine>
+        requires std::derived_from<T, IE::Core::Engine>
 
     static T *createEngine(std::string id, Args... args) {
         std::unique_lock<std::mutex> lock(m_enginesMutex);
@@ -34,7 +34,7 @@ public:
     }
 
     template<typename T>
-    requires std::derived_from<T, IE::Core::Engine>
+        requires std::derived_from<T, IE::Core::Engine>
 
     static T *getEngine(std::string id) {
         std::unique_lock<std::mutex> lock(m_enginesMutex);
