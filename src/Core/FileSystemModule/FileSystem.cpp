85690910d6517e6cbe5b08a53d1b1cdd67cfc8c6
PercentBoat4164 <thadd.schelp@gmail.com>
2022-11-22 14:02:14 -0500

    - Grabbed pre-discussed changes from shadows branch.

--- src/Core/FileSystemModule/FileSystem.cpp
+++ src/Core/FileSystemModule/FileSystem.cpp
@@ -4,48 +4,65 @@
 
 #include <filesystem>
 
-IE::Core::FileSystem::FileSystem(const std::filesystem::path &fileSystemPath) {
-    path = fileSystemPath.string();
+IE::Core::File *IE::Core::FileSystem::addFile(const std::filesystem::path &filePath) {
+    std::filesystem::path newPath(filePath);
+    makePathAbsolute(newPath);
+    createFolder(newPath.parent_path().string());
+    auto file = getFile(newPath);
+    if (file == nullptr) return &(*m_files.insert(std::make_pair(filePath.string(), File(newPath))).first).second;
+    return file;
 }
 
-IE::Core::File* IE::Core::FileSystem::addFile(const std::filesystem::path &filePath) {
-    std::filesystem::path newPath(path / filePath);
-    createFolder(newPath.parent_path().string());
-    return &(* files.insert(std::make_pair(filePath.string(), File(newPath))).first).second;
+std::filesystem::path &IE::Core::FileSystem::makePathAbsolute(std::filesystem::path &filePath) {
+    if (!filePath.string().starts_with(m_path.string())) filePath = m_path / filePath;
+    return filePath;
 }
 
 void IE::Core::FileSystem::createFolder(const std::filesystem::path &folderPath) const {
-    std::filesystem::create_directories(path / folderPath);
+    std::filesystem::create_directories(m_path / folderPath);
 }
 
 void IE::Core::FileSystem::exportData(const std::filesystem::path &filePath, const std::vector<char> &data) {
-    files.find(filePath.string())->second.write(data);
+    m_files.find(filePath.string())->second.write(data);
 }
 
 void IE::Core::FileSystem::deleteFile(const std::filesystem::path &filePath) {
-    files.erase(files.find(filePath.string()));
-    std::filesystem::remove(path / filePath);
+    m_files.erase(m_files.find(filePath.string()));
+    std::filesystem::remove(m_path / filePath);
 }
 
 void IE::Core::FileSystem::deleteDirectory(const std::filesystem::path &filePath) const {
-    if (std::filesystem::is_empty(path / filePath) && std::filesystem::is_directory(std::filesystem::path(path.string() + "/" + filePath.string())))
-        std::filesystem::remove(path / filePath);
+    if (std::filesystem::is_empty(m_path / filePath) && std::filesystem::is_directory(std::filesystem::path(m_path.string() + "/" + filePath.string())))
+        std::filesystem::remove(m_path / filePath);
 }
 
 void IE::Core::FileSystem::deleteUsedDirectory(const std::filesystem::path &filePath) {
     std::string testPath;
-    for (const auto &x : files) {
+    for (const auto &x : m_files) {
         testPath = x.second.path.string().substr(0, filePath.string().size());
-        if (testPath == filePath) files.erase(x.first);
+        if (testPath == filePath) m_files.erase(x.first);
     }
-    std::filesystem::remove_all(path / filePath);
+    std::filesystem::remove_all(m_path / filePath);
 }
 
 IE::Core::File *IE::Core::FileSystem::getFile(const std::filesystem::path &filePath) {
-    return &files.find(filePath.string())->second;
+    std::filesystem::path newPath(filePath);
+    makePathAbsolute(newPath);
+    auto iterator = m_files.find(newPath.string());
+    if (iterator == m_files.end()) return nullptr;
+    return &iterator->second;
 }
 
-// template<class T>
-// void FileSystem::importFile(T* data, File &file, unsigned int flags) {
-//     importer.import(data, file, flags);
-// }
+std::filesystem::path IE::Core::FileSystem::getBaseDirectory(const std::filesystem::path &t_path) {
+    return t_path;
+}
+
+void IE::Core::FileSystem::setBaseDirectory(const std::filesystem::path &t_path) {
+    m_path = t_path;
+    m_files.clear();
+    std::filesystem::recursive_directory_iterator directory{t_path};
+    for (auto entry : directory)
+        if (!entry.is_directory()) addFile(entry.path());
+}
+
+IE::Core::FileSystem::FileSystem() = default;
