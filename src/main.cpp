61e63f0b0c42d23da8bf70367fbe61b69f3a21a5
PercentBoat4164 <thadd.schelp@gmail.com>
2023-02-15 13:08:23 -0500

    - Fixed the fullscreen bug.

--- src/main.cpp
+++ src/main.cpp
@@ -13,60 +13,83 @@ int main(int argc, char **argv) {
     }
 
     IESettings settings     = IESettings();
-    auto      *renderEngine = IE::Core::Core::createEngine<IERenderEngine>("render engine", &settings);
+    auto      *renderEngine = IE::Core::Core::createEngine<IERenderEngine>("render engine", settings);
 
     IE::Input::InputEngine inputEngine{renderEngine->window};
     IE::Input::Keyboard   *keyboard = inputEngine.getAspect("keyboard");
-    keyboard->editActions(GLFW_KEY_W, [&](GLFWwindow *) {
-        renderEngine->camera.position +=
-          renderEngine->camera.front * renderEngine->frameTime * renderEngine->camera.speed;
-    });
-    keyboard->editActions(GLFW_KEY_A, [&](GLFWwindow *) {
-        renderEngine->camera.position -=
-          renderEngine->camera.right * renderEngine->frameTime * renderEngine->camera.speed;
-    });
-    keyboard->editActions(GLFW_KEY_S, [&](GLFWwindow *) {
-        renderEngine->camera.position -=
-          renderEngine->camera.front * renderEngine->frameTime * renderEngine->camera.speed;
-    });
-    keyboard->editActions(GLFW_KEY_D, [&](GLFWwindow *) {
-        renderEngine->camera.position +=
-          renderEngine->camera.right * renderEngine->frameTime * renderEngine->camera.speed;
-    });
-    keyboard->editActions(GLFW_KEY_SPACE, [&](GLFWwindow *) {
-        renderEngine->camera.position +=
-          renderEngine->camera.up * renderEngine->frameTime * renderEngine->camera.speed;
-    });
-    keyboard->editActions(GLFW_KEY_LEFT_SHIFT, [&](GLFWwindow *) {
-        renderEngine->camera.position -=
-          renderEngine->camera.up * renderEngine->frameTime * renderEngine->camera.speed;
-    });
-    keyboard->editActions({GLFW_KEY_LEFT_CONTROL, GLFW_PRESS}, [&](GLFWwindow *) {
-        renderEngine->camera.speed *= 6.0;
-    });
-    keyboard->editActions({GLFW_KEY_LEFT_CONTROL, GLFW_RELEASE}, [&](GLFWwindow *) {
-        renderEngine->camera.speed /= 6.0;
-    });
-    keyboard->editActions({GLFW_KEY_F11, GLFW_PRESS}, [&](GLFWwindow *) { renderEngine->toggleFullscreen(); });
+    keyboard->editActions(
+      GLFW_KEY_W,
+      [&](GLFWwindow *) {
+          renderEngine->camera.position +=
+            renderEngine->camera.front * renderEngine->frameTime * renderEngine->camera.speed;
+      }
+    );
+    keyboard->editActions(
+      GLFW_KEY_A,
+      [&](GLFWwindow *) {
+          renderEngine->camera.position -=
+            renderEngine->camera.right * renderEngine->frameTime * renderEngine->camera.speed;
+      }
+    );
+    keyboard->editActions(
+      GLFW_KEY_S,
+      [&](GLFWwindow *) {
+          renderEngine->camera.position -=
+            renderEngine->camera.front * renderEngine->frameTime * renderEngine->camera.speed;
+      }
+    );
+    keyboard->editActions(
+      GLFW_KEY_D,
+      [&](GLFWwindow *) {
+          renderEngine->camera.position +=
+            renderEngine->camera.right * renderEngine->frameTime * renderEngine->camera.speed;
+      }
+    );
+    keyboard->editActions(
+      GLFW_KEY_SPACE,
+      [&](GLFWwindow *) {
+          renderEngine->camera.position +=
+            renderEngine->camera.up * renderEngine->frameTime * renderEngine->camera.speed;
+      }
+    );
+    keyboard->editActions(
+      GLFW_KEY_LEFT_SHIFT,
+      [&](GLFWwindow *) {
+          renderEngine->camera.position -=
+            renderEngine->camera.up * renderEngine->frameTime * renderEngine->camera.speed;
+      }
+    );
+    keyboard->editActions(
+      {GLFW_KEY_LEFT_CONTROL, GLFW_PRESS},
+      [&](GLFWwindow *) { renderEngine->camera.speed *= 6.0; }
+    );
+    keyboard->editActions(
+      {GLFW_KEY_LEFT_CONTROL, GLFW_RELEASE},
+      [&](GLFWwindow *) { renderEngine->camera.speed /= 6.0; }
+    );
+    keyboard->editActions(
+      {GLFW_KEY_F11, GLFW_PRESS},
+      [&](GLFWwindow *) { renderEngine->queueToggleFullscreen(); }
+    );
     keyboard->editActions({GLFW_KEY_ESCAPE, GLFW_REPEAT}, [&](GLFWwindow *) {
         glfwSetWindowShouldClose(renderEngine->window, 1);
     });
 
-    std::shared_ptr<IEAsset> fbx(std::make_shared<IEAsset>());
-    fbx->filename = "res/assets/AncientStatue/models/ancientStatue.fbx";
-    fbx->position = {2, 1, 0};
-    fbx->addAspect(new IERenderable{});
-    renderEngine->addAsset(fbx);
-    std::shared_ptr<IEAsset> obj = std::make_shared<IEAsset>();
-    obj->filename                = "res/assets/AncientStatue/models/ancientStatue.obj";
-    obj->addAspect(new IERenderable{});
-    obj->position = {0, 1, 0};
-    renderEngine->addAsset(obj);
-    std::shared_ptr<IEAsset> glb = std::make_shared<IEAsset>();
-    glb->filename                = "res/assets/AncientStatue/models/ancientStatue.glb";
-    glb->addAspect(new IERenderable{});
-    glb->position = {-2, 1, 0};
-    renderEngine->addAsset(glb);
+    //    std::shared_ptr<IEAsset> fbx(std::make_shared<IEAsset>());
+    //    fbx->filename = "res/assets/AncientStatue/models/ancientStatue.fbx";
+    //    fbx->position = {2, 1, 0};
+    //    fbx->addAspect(new IERenderable{});
+    //    renderEngine->addAsset(fbx);
+    //    std::shared_ptr<IEAsset> obj = std::make_shared<IEAsset>();
+    //    obj->filename                = "res/assets/AncientStatue/models/ancientStatue.obj";
+    //    obj->addAspect(new IERenderable{});
+    //    obj->position = {0, 1, 0};
+    //    renderEngine->addAsset(obj);
+    //    std::shared_ptr<IEAsset> glb = std::make_shared<IEAsset>();
+    //    glb->filename                = "res/assets/AncientStatue/models/ancientStatue.glb";
+    //    glb->addAspect(new IERenderable{});
+    //    glb->position = {-2, 1, 0};
+    //    renderEngine->addAsset(glb);
     std::shared_ptr<IEAsset> floor = std::make_shared<IEAsset>();
     floor->filename                = "res/assets/DeepslateFloor/models/DeepslateFloor.fbx";
     floor->addAspect(new IERenderable{});
