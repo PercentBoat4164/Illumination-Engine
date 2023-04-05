273b2726b58ebb479017e7911f515e9ea849ceb0
Luca Miceli <luca.miceli@comcast.net>
2023-04-05 09:47:31 -0400

    New thread Pool changes

--- src/main.cpp
+++ src/main.cpp
@@ -5,16 +5,22 @@
 
 #include <iostream>
 #include <SDL.h>
+#include <thread>
 
-int main(int argc, char **argv) {
-    if (argc >= 1) {
-        auto              programLocation  = std::filesystem::path(argv[0]);
-        std::string const resourceLocation = programLocation.parent_path().string();
-        IE::Core::Core::getInst(resourceLocation);
-    }
-
+IE::Core::Threading::CoroutineTask<void> illuminationEngine() {
     IESettings settings     = IESettings();
     auto      *renderEngine = IE::Core::Core::createEngine<IERenderEngine>("render engine", &settings);
+
+    IE::Core::Core::getLogger()->log(std::to_string(std::hash<std::thread::id>{}(std::this_thread::get_id())));
+
+    co_await IE::Core::Core::getThreadPool()->ensureThread(IE::Core::Threading::ThreadType::IE_THREAD_TYPE_MAIN_THREAD);
+    IE::Core::Core::getLogger()->log(std::to_string(std::hash<std::thread::id>{}(std::this_thread::get_id())));
+    // NOW EXECUTING ON THE MAIN THREAD.
+    co_await IE::Core::Core::getThreadPool()->ensureThread(IE::Core::Threading::ThreadType::IE_THREAD_TYPE_WORKER_THREAD);
+    IE::Core::Core::getLogger()->log(std::to_string(std::hash<std::thread::id>{}(std::this_thread::get_id())));
+    // NOW EXECUTING ON A THREAD POOL MANAGED THREAD. Note that this thread is not *necessarily* the same as the worker thread before.
+
+    // To submit an entire job use: `auto job = IE::Core::Core::getThreadPool()->submitToMainThread(illuminationEngine);`
 
 
     //    IE::Input::InputEngine inputEngine{renderEngine->window};
